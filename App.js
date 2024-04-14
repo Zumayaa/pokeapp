4398c003203cc8f60c8ba38de257bd0111d86328
Zumayaa <abzu_21@alu.uabcs.mx>
2024-04-14 13:29:18 -0700

    react app

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-web';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Lo que es</Text>
+      <StatusBar style="auto" />
+      <Button title="Learn More" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
